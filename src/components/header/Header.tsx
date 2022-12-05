744da5ec430a678c7b3aac003a8bfdc5ab108b2a
Anrsgrl <esgerlianar003@gmail.com>
2022-12-05 13:24:11 +0300

    nearly done

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import './header.scss';
-import logo from './logo.png';
+import logo from '../../assets/images/WhiteLogo.svg';
 import icon1 from '../../assets/images/WhiteSearch.svg'
 import icon2 from '../../assets/images/WhiteAccount.svg'
 import icon3 from '../../assets/images/WhiteCart.svg'
@@ -25,29 +25,29 @@ function Header(){
                 <div className="logo-field">
                     <div className="header-logo">
                         <Link to="/">
-                            <img src={logo} alt="Our logo" className='logo-image img-responsive' />
+                            <img src={logo} alt="Our logo" className={color ? 'scroll-fill logo-image img-responsive ' : 'logo-image img-responsive'} />
                         </Link>
                     </div>
                 </div>
                 <div className="header-navbar">
                     <ul className='navbar-menu'>
                         <li className='menu-item'>
-                            <Link to="/" className='menu-item-href'>
+                            <Link to="/" className={color ? 'menu-item-href' : ''}>
                                 Home
                             </Link>
                         </li>
                         <li className='menu-item'>
-                            <Link to="/" className='menu-item-href'>
+                            <Link to="/" className={color ? 'menu-item-href' : ''}>
                                 Shop
                             </Link>
                         </li>
                         <li className='menu-item'>
-                            <Link to="/" className='menu-item-href'>
+                            <Link to="/" className={color ? 'menu-item-href' : ''}>
                                 Contact
                             </Link>
                         </li>
                         <li className='menu-item pages-menu'>
-                            <Link to="/" className='menu-item-href'>
+                            <Link to="/" className={color ? 'menu-item-href' : ''}>
                                 Pages
                             </Link>
                             <ul className='sub-menu'>
@@ -74,7 +74,7 @@ function Header(){
                             </ul>
                         </li>
                         <li className='menu-item'>
-                            <Link to="/" className='menu-item-href'>
+                            <Link to="/" className={color ? 'menu-item-href' : ''}>
                                 About
                             </Link>
                         </li>
@@ -83,17 +83,17 @@ function Header(){
                 <div className="header-icons">
                     <div className="icons-element icons-search">
                         <Link to="/" className='search-btn'>
-                            <img src={icon1} alt="Search" />
+                            <img src={icon1} alt="Search" className={color ? 'scroll-fill' : ''} />
                         </Link>
                     </div>
                     <div className="icons-element icons-account">
                         <Link to="/" className='account-btn'>
-                            <img src={icon2} alt="Account" />
+                            <img src={icon2} alt="Account" className={color ? 'scroll-fill' : ''} />
                         </Link>
                     </div>
                     <div className="icons-element icons-cart">
                         <Link to="/" className='Cart-btn'>
-                            <img src={icon3} alt="Cart" />
+                            <img src={icon3} alt="Cart" className={color ? 'scroll-fill' : ''} />
                         </Link>
                     </div>
                 </div>
