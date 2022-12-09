b42f7b4fc9eb83fb0b5005cbed8ad1ee668d6e00
M.Sakina <sakhinamammadzade0406@gmail.com>
2022-12-09 23:10:56 +0400

    banner and slider are finished

--- src/pages/shop/Shop.tsx
+++ src/pages/shop/Shop.tsx
@@ -1,5 +1,15 @@
+import Banner from '../../components/banner/Banner';
 import React from 'react';
 
 export default function Shop() {
-  return <div>Shop</div>;
+  return (
+    <section className="Shop">
+      <Banner
+        title="Shop"
+        pageName="Shop"
+        coverPhoto="https://casona.familab.net/wp-content/uploads/2021/09/heading_bg_dark.jpg"
+        sliderExist={true}
+      />
+    </section>
+  );
 }
