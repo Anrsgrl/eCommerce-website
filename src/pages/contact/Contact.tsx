0dcffb09516e5e8291b2f8c549338d947f665daa
Nargiz <lmnargizl@gmail.com>
2022-12-11 20:14:31 +0400

    unfinished

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import "./contact.scss"
+import logo from './logo.png'
+import email from './email.png'
+import location from './location.png'
+
 
 export default function Contact() {
     return (
         <div>
-            
+            <div className='three-divs'>
+                <div className='contact-box'>
+                    <img src={logo} alt="phone" />
+                    <p className='phone'>Phone</p>
+                    <p className='call'>Call us: 0333 015 0000</p>
+                </div>
+                <div className='contact-box'>
+                    <img src={location} alt="location" />
+                    <p className='phone'>Live chat</p>
+                    <p className='call'>Click here to launch live chat</p>
+                </div>
+                <div className='contact-box'>
+                    <img src={email} alt="email" />
+                    <p className='phone'>Email</p>
+                    <p className='call'>Email us at: contact@familab.net</p>
+                </div>
+            </div>
         </div>
     )
 }
