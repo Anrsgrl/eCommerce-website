b36a8aba37c5e572ddc5cd0541f3fa3db5d85cd4
Anrsgrl <esgerlianar003@gmail.com>
2022-12-16 20:54:15 +0300

    30% done

---
+++ src/pages/home/components/HomeServices/HomeServices.tsx
@@ -0,0 +1,18 @@
+import './HomeServices.scss';
+import React from 'react';
+
+export default function HomeServices() {
+  return (
+    <div className="Services-field">
+      <div className="Services-items">
+        <div className="Services-item">
+          <div className="ServicesItem-img"></div>
+          <div className="ServicesItem-content">
+            <p className="SI-title">Free Shipping</p>
+            <p className="SI-title2">Free worldwide shipping on all Area order above $100</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
