fd2e6a72f567f845d7a920103fbf98d9611186a6
Anrsgrl <esgerlianar003@gmail.com>
2022-12-17 02:05:03 +0300

    Subscribe part done

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,6 +3,7 @@ import DailyEssentials from './components/DailyEssentials/DailyEssentials';
 import Hanpicked from './components/Hanpicked/Hanpicked';
 import HomeServices from './components/HomeServices/HomeServices';
 import HomeSlider from './components/HomeSlider/HomeSlider';
+import HomeSubscribe from './components/HomeSubscribe/HomeSubscribe';
 import React from 'react';
 import SaleUp from './components/SaleUp/SaleUp';
 
@@ -14,8 +15,7 @@ export default function Home() {
       <SaleUp />
       <Hanpicked />
       <HomeServices />
-      <div className="bg"></div>
-      <div className="bg"></div>
+      <HomeSubscribe />
     </>
   );
 }
