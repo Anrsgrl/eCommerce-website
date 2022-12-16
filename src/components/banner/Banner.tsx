378d966e55e0aa7dddc62b3295d26cc9a5e62e91
Anrsgrl <esgerlianar003@gmail.com>
2022-12-16 13:31:32 +0300

    banner name fixed

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -1,4 +1,4 @@
-import './Banner.scss';
+import './banner.scss';
 // import { ChevronRight } from 'react-bootstrap-icons';
 import { ChevronRight } from 'react-bootstrap-icons';
 import { Link } from 'react-router-dom';
