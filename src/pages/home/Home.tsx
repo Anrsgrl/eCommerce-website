fd8b3994a41a2e131bee7ce01306a13b050c5f2f
Anrsgrl <esgerlianar003@gmail.com>
2022-12-16 17:03:55 +0300

    done +responsive

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,12 +2,14 @@ import './home.scss';
 import DailyEssentials from './components/DailyEssentials/DailyEssentials';
 import HomeSlider from './components/HomeSlider/HomeSlider';
 import React from 'react';
+import SaleUp from './components/SaleUp/SaleUp';
 
 export default function Home() {
   return (
     <>
       <HomeSlider />
       <DailyEssentials />
+      <SaleUp />
       <div className="bg"></div>
       <div className="bg"></div>
     </>
