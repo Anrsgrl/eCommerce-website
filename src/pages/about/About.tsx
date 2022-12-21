bbbe27e5e7a03009819e851285b0ce433f4d8548
Anrsgrl <esgerlianar003@gmail.com>
2022-12-22 02:01:08 +0300

    pulled from main

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,21 +1,17 @@
 import '../about/about.scss';
-<<<<<<< HEAD
 import Banner from '../../components/banner/Banner';
-import React from 'react';
-import Handcrafted from './components/Handcrafted/Handcrafted';
-=======
 import Handcrafted from './components/Handcrafted/Handcrafted';
 import React from 'react';
-import Banner from '../../components/Banner/Banner';
->>>>>>> 31a2a905c36e38cd377f0412a3130e8c06b33e22
+
 export default function About() {
   return (
     <>
       <Banner
-          title="About" 
-          pageName="About"
-          coverPhoto="https://casona.familab.net/wp-content/uploads/2021/08/blog-bgf.jpg"
-          sliderExist={false} />
+        title="About"
+        pageName="About"
+        coverPhoto="https://casona.familab.net/wp-content/uploads/2021/08/blog-bgf.jpg"
+        sliderExist={false}
+      />
       <Handcrafted />
     </>
   );
--- src/pages/cart/Cart.tsx
+++ src/pages/cart/Cart.tsx
@@ -2,7 +2,6 @@ import Banner from '../../components/banner/Banner';
 import React from 'react';
 import bannerBg from '../../assets/images/bannerBg.jpg';
 
-
 export default function Cart() {
   return <Banner title="Whishlist" pageName="Whishlist" coverPhoto={bannerBg} sliderExist={false} />;
 }
