ede87d8c86a2626922b72d06b36304efed6d277a
Anton <Negodyaj@users.noreply.github.com>
2022-12-22 01:09:50 +0300

    Revert "OrderTracking added"

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './index.scss';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
--- src/pages/wishlist/Whishlist.tsx
+++ src/pages/wishlist/Whishlist.tsx
@@ -1,7 +1,5 @@
-import Banner from '../../components/banner/Banner';
 import React from 'react';
-import bannerBg from '../../assets/images/bannerBg.jpg';
 
 export default function Whishlist() {
-  return <Banner title="Whishlist" pageName="Whishlist" coverPhoto={bannerBg} sliderExist={false} />;
+  return <div>Whishlist</div>;
 }
