27506900c924e19195403df110caf39d598d5e8f
Anrsgrl <esgerlianar003@gmail.com>
2022-12-21 21:08:16 +0300

    OrderTracking added

--- src/api/ShopApi.js
+++ src/api/ShopApi.js
@@ -1,15 +1,14 @@
-import axios from 'axios'
-import { Base_Url } from './Config'
+import { Base_Url } from './Config';
+import axios from 'axios';
 
-
-const shopApi={
-    getProducts :async function (){
-        const{data}=await axios(`${Base_Url}products`);
-        return data
-    },
-    getProductsById:async function(id){
-        const {data}=await axios (`${Base_Url}produtcs/${id}`);
-        return data
-    }
-}
-export default shopApi
+const shopApi = {
+  getProducts: async function () {
+    const { data } = await axios(`${Base_Url}products`);
+    return data;
+  },
+  getProductsById: async function (id) {
+    const { data } = await axios(`${Base_Url}produtcs/${id}`);
+    return data;
+  },
+};
+export default shopApi;
